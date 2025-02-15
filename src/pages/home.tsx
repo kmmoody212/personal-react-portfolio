048ea69d1379f5ddd44580cddf0fafa23779f0fa
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2025-02-15 14:16:29 -0500

    Made basic framing for home page.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,18 @@
-
+import './css/homepage.css';
+import fox from '../assets/ht_pudding_the_fox_03_mt_140821_16x9_992.jpg';
 
 const Home = () => {
     return (
         <>
-        <section>
+        <section className="section">
         <div>MOODY ENTERPRISES</div>
-        <div>IMG</div>
-        <p>Building Better Applications</p>
-        <ul>
-            <li>GH</li>
-            <li>IN</li>
-            <li>IG</li>
-        </ul>
+        <img src={fox}></img>
+        <p>"Building Better Applications"</p>
+        <div className="home-links">
+            <a href="#">GH</a>
+            <a href="#">IN</a>
+            <a href="#">IG</a>
+        </div>
         </section>
         </>
     )
